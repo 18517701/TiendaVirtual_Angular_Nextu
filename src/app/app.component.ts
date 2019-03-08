f09489b6481341393ee220b01f23cdb0601ba7f8
abnerord <abner.ord@gmail.com>
2019-03-07 23:41:38 -0600

    primera fase terminada
    se termino la primera fase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {LoginService} from './login.service'
+import { log } from 'util';
+import { usuario } from './usuarios';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TiendaVirtual';
+  
+  validacion = false;
+
+
+  constructor(private Servicio:LoginService){}
+
+
+  onSubmit(form){
+
+    this.Servicio.login().subscribe((data: Response)=>{
+     
+      this.validacion = true;
+
+      for(let key in data){
+
+        if(data[key].email == form.value.nombre && data[key].password == form.value.password){
+          this.validacion = false;
+        }
+      }
+      
+
+      console.log(this.validacion);
+      //var usuarios = JSON.parse(JSON.stringify(data));
+      
+      //var validacion = false;
+    }
+  );
+
+    
+  }
+
+
 }
